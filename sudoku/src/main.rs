816f96b7e42528ee364879b6d7d5a8f34b9f195f
Edgar Nova <ragnarok540@gmail.com>
2023-03-25 18:49:12 -0500

    Sudoku

--- sudoku/src/main.rs
+++ sudoku/src/main.rs
@@ -1,13 +1,46 @@
-use piston::WindowSettings;
-use glutin_window::GlutinWindow;
+#![deny(missing_docs)]
+
+//! A sudoku game.
+
+pub use crate::gameboard::Gameboard;
+pub use crate::gameboard_controller::GameboardController;
+pub use crate::gameboard_view::{GameboardView, GameboardViewSettings};
+
+mod gameboard;
+mod gameboard_controller;
+mod gameboard_view;
+
+use piston::{WindowSettings, EventLoop, RenderEvent};
 use piston::event_loop::{EventSettings, Events};
+use glutin_window::GlutinWindow;
+use opengl_graphics::{OpenGL, GlGraphics};
+
 
 fn main() {
-    let settings = WindowSettings::new("Sudoku", (640, 480)).exit_on_esc(true);
+    let opengl = OpenGL::V3_2;
+    let settings = WindowSettings::new("Sudoku", (640, 480))
+        .exit_on_esc(true)
+        .graphics_api(opengl);
     let mut window: GlutinWindow = settings.build().expect("Could not create window");
-    let mut events = Events::new(EventSettings::new());
+    let mut events = Events::new(EventSettings::new()).lazy(true);
+    let mut gl = GlGraphics::new(opengl);
 
-    //while let some
+    let gameboard = Gameboard::new();
+    let mut gameboard_controller = GameboardController::new(gameboard);
+    let gameboard_view_settings = GameboardViewSettings::new();
+    let gameboard_view = GameboardView::new(gameboard_view_settings);
+
+    while let Some(e) = events.next(&mut window) {
+        gameboard_controller.event(gameboard_view.settings.position, gameboard_view.settings.size, &e);
+        if let Some(args) = e.render_args() {
+            gl.draw(args.viewport(), |c, g| {
+                use graphics::{clear};
+
+                clear([1.0; 4], g);
+                gameboard_view.draw(&gameboard_controller, &c, g);
+            });
+        }
+    }
 
     println!("{}", settings.get_exit_on_esc());
 }
